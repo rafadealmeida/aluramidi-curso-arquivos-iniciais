2c9ed137a108bcb59e8d57f02594691cf5708bc0
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2022-02-14 16:04:06 -0300

    Acrescentando JavaScript

---
+++ main.js
@@ -0,0 +1,17 @@
+var pom = document.querySelector(".tecla_pom");
+var clap = document.querySelector(".tecla_clap");
+var tim = document.querySelector(".tecla_tim");
+
+var puff = document.querySelector(".tecla_puf");
+var splash = document.querySelector(".tecla_splash");
+var toim = document.querySelector(".tecla_toim");
+
+var psh = document.querySelector(".tecla_ psh");
+var tic = document.querySelector(".tecla_ tic");
+var tom = document.querySelector(".tecla_tom");
+
+
+var som_pom = new Audio(pom);
+
+pom.addEventListener('click', som_pom.play())
+
