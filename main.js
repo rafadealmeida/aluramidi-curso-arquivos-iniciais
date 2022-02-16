fbb9eefdd899f72f32493999dac5ba5c5cf98c9c
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2022-02-16 17:37:37 -0300

    update

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
-var pom = document.querySelector(".tecla_pom");
-var clap = document.querySelector(".tecla_clap");
-var tim = document.querySelector(".tecla_tim");
-
-var puff = document.querySelector(".tecla_puf");
-var splash = document.querySelector(".tecla_splash");
-var toim = document.querySelector(".tecla_toim");
-
-var psh = document.querySelector(".tecla_ psh");
-var tic = document.querySelector(".tecla_ tic");
-var tom = document.querySelector(".tecla_tom");
+const somTeclas = document.querySelectorAll('.tecla_som');
 
 
-var som_pom = new Audio(pom);
+function tocaSom(indice) {
+    somTeclas[indice].play(); 
 
-pom.addEventListener('click', som_pom.play())
 
+}
+
+
+const listaTeclas = document.querySelectorAll('.tecla');
+
+var indice = 0;
+
+listaTeclas[0].onclick = somTeclas[0].tocaSom;
+
+
+for (let i = 0; i < somTeclas.length; i++) {
+    const element = somTeclas[i];
+    
+}
