394b9d4aa18f05dbecbf4ef542a5c6f36635643e
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2022-02-21 17:55:20 -0300

    utilizando string dinâmicas

--- main.js
+++ main.js
@@ -6,28 +6,27 @@ function tocaSom(indice) {
 }
 
 
+function tocaTecla (id){
+    document.querySelector(id).play();
+
+}
+
 
 
 const listaTeclas = document.querySelectorAll('.tecla');
 
 //Usei tanto as teclas quanto os audios em estilo de lista(array) e usei os indice delas para tocar som, vinculada a tecla.
 
-for (let i = 0; i < somTeclas.length; i++) {
+for (let i = 0; i < listaTeclas.length; i++) {
     
-    
-    
+    const instrumento = listaTeclas[i].classList[1];
+    const idDaTecla = `#som_${instrumento}`;
+
+
+
     listaTeclas[i].onclick = function() {
-        
+
         tocaTecla(idDaTecla);
     } 
-    
-    
-    var instrumento = listaTeclas[i].classList[1];
-    var idDaTecla = `#som_${instrumento}`;
-}
-
-
-function tocaTecla (id){
-    document.querySelector(id).play();
 
 }
