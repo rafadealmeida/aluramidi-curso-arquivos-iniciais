6bb553ddd22a801e3cd4aafedffba8a35866957a
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2022-03-01 13:52:10 -0300

    Colocando uma condicional

--- main.js
+++ main.js
@@ -30,8 +30,12 @@ for (let i = 0; i < listaTeclas.length; i++) {
     } 
 
 
-    listaTeclas[i].onkeydown = function () {
-        listaTeclas[i].classList.add('ativa')
+    listaTeclas[i].onkeydown = function (evento) {
+
+        if(evento.code == "Space" || evento.code == "Enter"){
+            
+            listaTeclas[i].classList.add('ativa')
+        }
     }
     listaTeclas[i].onkeyup = function () {
         listaTeclas[i].classList.remove('ativa')
